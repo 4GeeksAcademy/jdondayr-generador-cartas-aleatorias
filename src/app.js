7f04ec65eadc2f048b7974396521d80dddbfed9b
Juan <jdondayr@gmail.com>
2026-05-19 11:52:06 +0200

    Project done: every time user refresh the web, the app generates a random card

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
+// Variables
+const number = document.querySelector("#number");
+let icons = document.querySelectorAll(".icon");
 
+const numbersPossibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]; // Posibilidad de números
+const iconsPossibilities = ["♦", "♥", "♠", "♣"]; // Posibilidad de iconos
+const cardClasses = []
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Get Random Element Function
+const getRandomElementFromArray = (array) => {
+  return array[Math.floor(Math.random() * array.length)]
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// Onload function
+window.addEventListener("load", (event) => {
+  number.innerHTML = getRandomElementFromArray(numbersPossibilities);
+  const randomIcon = getRandomElementFromArray(iconsPossibilities)
+  icons.forEach(icon => {
+    if (randomIcon === "♦" || randomIcon === "♥") icon.style.color = "red"; // Si es de diamantes o corazones, los iconos son rojos
+    icon.innerHTML = randomIcon;
+  });
+})
