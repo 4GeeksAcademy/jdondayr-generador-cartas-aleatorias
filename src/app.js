c4bc1066cfd39464d5f2277759f9c136374c0998
Juan <jdondayr@gmail.com>
2026-05-20 15:59:59 +0200

    New feature: now user can change the background color selecting one from the list

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@
 const number = document.querySelector("#number");
 const icons = document.querySelectorAll(".icon");
 const generateRandomCardButton = document.querySelector("#generateRandomCardButton");
+const backgroundColorSelect = document.querySelector("#backgroundColorSelect");
+const container = document.querySelector("#container");
 
 const numbersPossibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]; // Posibilidad de números
 const iconsPossibilities = ["♦", "♥", "♠", "♣"]; // Posibilidad de iconos
+const numberColorsPossibilities = ["blue", "green", "black", "yellow", "red"];
 
 // Get Random Element Function
 const getRandomElementFromArray = (array) => {
@@ -14,6 +17,7 @@ const getRandomElementFromArray = (array) => {
 // Generate random card button function
 generateRandomCardButton.addEventListener("click", (event) => {
   number.innerHTML = getRandomElementFromArray(numbersPossibilities);
+  number.style.color = getRandomElementFromArray(numberColorsPossibilities);
   const randomIcon = getRandomElementFromArray(iconsPossibilities)
   icons.forEach(icon => {
     if (randomIcon === "♦" || randomIcon === "♥") icon.style.color = "red"; // Si es de diamantes o corazones, los iconos son rojos
@@ -21,3 +25,25 @@ generateRandomCardButton.addEventListener("click", (event) => {
     icon.innerHTML = randomIcon;
   });
 });
+
+// Background color function
+backgroundColorSelect.addEventListener("change", () => {
+  switch (backgroundColorSelect.value) {
+    case "green":
+      container.style = "background: green;";
+    break;
+    case "red":
+      container.style = "background: red;";
+    break;
+    case "blue":
+      container.style = "background: blue;";
+    break;
+    case "yellow":
+      container.style = "background: yellow;";
+    break;
+    case "black":
+      container.style = "background: black;";
+    break;
+
+  }
+})
